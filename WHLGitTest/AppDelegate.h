dd7479561efb009b026071ff5f762d07daedb67c
henghadawang <867561903@qq.com>
2017-01-20 19:02:34 +0800

    Initial Commit

---
+++ WHLGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHLGitTest
+//
+//  Created by LuWei on 17/1/20.
+//  Copyright © 2017年 LuWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHLGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHLGitTest
+//
+//  Created by LuWei on 17/1/20.
+//  Copyright © 2017年 LuWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
